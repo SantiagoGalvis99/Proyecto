da9b1b2926bff424d847bc6229d3ef523e2ac83e
SantiagoGalvis99 <sg644477@gmail.com>
2024-11-14 09:39:33 -0500

    update

---
+++ assets/js/formulario.js
@@ -0,0 +1,57 @@
+function getURLParams() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const productId = urlParams.get('product_id');
+    const price = parseFloat(urlParams.get('price'));
+ 
+    let productName = 'Producto desconocido';
+    switch (productId) {
+        case '1':
+            productName = 'Planta 1';
+            break;
+        case '2':
+            productName = 'Planta 2';
+            break;
+        case '3':
+            productName = 'Planta 3';
+            break;
+        case '4':
+            productName = 'Planta 4';
+            break;
+        case '5':
+            productName = 'Planta 5';
+            break; 
+    
+        case '6':
+            productName = 'Planta 6';
+            break;    
+    }
+ 
+    document.getElementById('product-name').textContent = productName;
+    document.getElementById('price').textContent = price;
+    document.getElementById('total-price').textContent = price;
+ }
+ 
+ let cantidad = 1;
+ let price = parseFloat(document.getElementById('price').textContent);
+ 
+ function updateQuantity(amount) {
+    let price = parseFloat(document.getElementById('price').textContent);
+    cantidad += amount;
+    if (cantidad < 1) cantidad = 1;  
+ 
+    document.getElementById('cantidad-display').textContent = cantidad;
+ 
+    const totalPrice = price * cantidad;
+    document.getElementById('total-price').textContent = totalPrice;
+ }
+ document.getElementById('checkout-form').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+    document.getElementById('thank-you-modal').style.display = 'block';
+    document.getElementById('overlay').style.display = 'block';
+ 
+    const productName = document.getElementById('product-name').textContent;
+    const totalPrice = parseFloat(document.getElementById('total-price').textContent);
+    document.getElementById('thank-you-message').textContent = `¡Gracias por comprar ${cantidad} ${productName}! Total: $${totalPrice.toFixed(2)}`;
+ });
+ 
+ getURLParams();

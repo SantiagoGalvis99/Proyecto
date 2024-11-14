97f0dc6d5cb63506235e533222edeaf21dfc5203
mateo89222 <mateodavid.sa@gmail.com>
2024-11-14 09:18:51 -0500

    carrucel de imagenes

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,22 @@
 document.getElementById("nav-toggle").addEventListener("click", function(){
     document.querySelector("nav").classList.toggle("nav-open");
 });
+let currentIndex = 0;
+
+document.querySelector('.prev-button').addEventListener('click', () => {
+   navigate(-1);
+});
+
+document.querySelector('.next-button').addEventListener('click', () => {
+   navigate(1);
+});
+
+function navigate(direction) {
+   const galleryContainer = document.querySelector('.gallery-container');
+   const totalImages = document.querySelectorAll('.gallery-item').length;
+
+   currentIndex = (currentIndex + direction + totalImages) % totalImages;
+   const offset = -currentIndex * 100;
+
+   galleryContainer.style.transform = `translateX(${offset}%)`;
+}
